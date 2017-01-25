0757fdab5e3052414a20dd3dac1aac678021a572
Liang Peng <penliang@seas.upenn.edu>
2017-01-25 14:09:58 -0500

    ds pandas intro

--- pandastest.py
+++ pandastest.py
@@ -1,53 +1,49 @@
-import numpy as np
-import pandas as pd
+from pandas import DataFrame, Series
 
-'''
-The following code is to help you play with the concept of Dataframe in Pandas.
+#################
+# Syntax Reminder:
+#
+# The following code would create a two-column pandas DataFrame
+# named df with columns labeled 'name' and 'age':
+#
+# people = ['Sarah', 'Mike', 'Chrisna']
+# ages  =  [28, 32, 25]
+# df = DataFrame({'name' : Series(people),
+#                 'age'  : Series(ages)})
 
-You can think of a Dataframe as something with rows and columns. It is
-similar to a spreadsheet, a database table, or R's data.frame object.
+def create_dataframe():
+    '''
+    Create a pandas dataframe called 'olympic_medal_counts_df' containing
+    the data from the table of 2014 Sochi winter olympics medal counts.
 
-*This playground is inspired by Greg Reda's post on Intro to Pandas Data Structures:
-http://www.gregreda.com/2013/10/26/intro-to-pandas-data-structures/
-'''
+    The columns for this dataframe should be called
+    'country_name', 'gold', 'silver', and 'bronze'.
 
-'''
-To create a dataframe, you can pass a dictionary of lists to the Dataframe
-constructor:
-1) The key of the dictionary will be the column name
-2) The associating list will be the values within that column.
-'''
-# Change False to True to see Dataframes in action
-if False:
-    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
-            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
-                     'Lions', 'Lions'],
-            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
-            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
-    football = pd.DataFrame(data)
-    print (football)
+    There is no need to  specify row indexes for this dataframe
+    (in this case, the rows will automatically be assigned numbered indexes).
 
-'''
-Pandas also has various functions that will help you understand some basic
-information about your data frame. Some of these functions are:
-1) dtypes: to get the datatype for each column
-2) describe: useful for seeing basic statistics of the dataframe's numerical
-   columns
-3) head: displays the first five rows of the dataset
-4) tail: displays the last five rows of the dataset
-'''
-# Change False to True to see these functions in action
-if False:
-    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
-            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
-                     'Lions', 'Lions'],
-            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
-            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
-    football = pd.DataFrame(data)
-    print (football.dtypes)
-    print ("")
-    print (football.describe())
-    print ("")
-    print (football.head())
-    print ("")
-    print (football.tail())
+    You do not need to call the function in your code when running it in the
+    browser - the grader will do that automatically when you submit or test it.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    # your code here
+    data = {'country_name': Series(countries), 'gold': Series(gold), \
+			'silver': Series(silver), 'bronze': Series(bronze)}
+    olympic_medal_counts_df = DataFrame(data)
+
+    return olympic_medal_counts_df
+
+df = create_dataframe()
+
+print(df[df['gold'] == 1])

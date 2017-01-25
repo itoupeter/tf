94a7a5df230cf2c9dc08a7d73f76794d87d919cf
Liang Peng <penliang@seas.upenn.edu>
2017-01-25 13:08:51 -0500

    plot

--- pytest.py
+++ pytest.py
@@ -1,3 +1,4 @@
+import webbrowser
 
 class Movie():
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -6,6 +7,9 @@ class Movie():
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+
 toy_story = Movie(
 	"Toy Story",
 	"A toy story",
@@ -13,3 +17,4 @@ toy_story = Movie(
 	"http://...")
 
 print(toy_story.storyline)
+toy_story.show_trailer();

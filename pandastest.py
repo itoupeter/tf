903a2f9ad0166fd6364378f1123845b9c39ef33a
Liang Peng <penliang@seas.upenn.edu>
2017-01-25 23:42:48 -0500

    ds titanic

--- pandastest.py
+++ pandastest.py
@@ -1,40 +1,76 @@
-import numpy as np
-from pandas import DataFrame, Series
+import numpy
+import pandas
+# import statsmodels.api as sm
 
-
-def numpy_dot():
+def simple_heuristic(file_path):
     '''
-    Imagine a point system in which each country is awarded 4 points for each
-    gold medal,  2 points for each silver medal, and one point for each
-    bronze medal.
+    In this exercise, we will perform some rudimentary practices similar to those of
+    an actual data scientist.
 
-    Using the numpy.dot function, create a new dataframe called
-    'olympic_points_df' that includes:
-        a) a column called 'country_name' with the country name
-        b) a column called 'points' with the total number of points the country
-           earned at the Sochi olympics.
+    Part of a data scientist's job is to use her or his intuition and insight to
+    write algorithms and heuristics. A data scientist also creates mathematical models
+    to make predictions based on some attributes from the data that they are examining.
 
-    You do not need to call the function in your code when running it in the
-    browser - the grader will do that automatically when you submit or test it.
+    We would like for you to take your knowledge and intuition about the Titanic
+    and its passengers' attributes to predict whether or not the passengers survived
+    or perished. You can read more about the Titanic and specifics about this dataset at:
+    http://en.wikipedia.org/wiki/RMS_Titanic
+    http://www.kaggle.com/c/titanic-gettingStarted
+
+    In this exercise and the following ones, you are given a list of Titantic passengers
+    and their associated information. More information about the data can be seen at the
+    link below:
+    http://www.kaggle.com/c/titanic-gettingStarted/data.
+
+    For this exercise, you need to write a simple heuristic that will use
+    the passengers' gender to predict if that person survived the Titanic disaster.
+
+    You prediction should be 78% accurate or higher.
+
+    Here's a simple heuristic to start off:
+       1) If the passenger is female, your heuristic should assume that the
+       passenger survived.
+       2) If the passenger is male, you heuristic should
+       assume that the passenger did not survive.
+
+    You can access the gender of a passenger via passenger['Sex'].
+    If the passenger is male, passenger['Sex'] will return a string "male".
+    If the passenger is female, passenger['Sex'] will return a string "female".
+
+    Write your prediction back into the "predictions" dictionary. The
+    key of the dictionary should be the passenger's id (which can be accessed
+    via passenger["PassengerId"]) and the associated value should be 1 if the
+    passenger survied or 0 otherwise.
+
+    For example, if a passenger is predicted to have survived:
+    passenger_id = passenger['PassengerId']
+    predictions[passenger_id] = 1
+
+    And if a passenger is predicted to have perished in the disaster:
+    passenger_id = passenger['PassengerId']
+    predictions[passenger_id] = 0
+
+    You can also look at the Titantic data that you will be working with
+    at the link below:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/titanic_data.csv
     '''
 
-    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
-                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
-                 'Austria', 'France', 'Poland', 'China', 'Korea',
-                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
-                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
-                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+    predictions = {}
+    df = pandas.read_csv(file_path)
+    for passenger_index, passenger in df.iterrows():
+        passenger_id = passenger['PassengerId']
 
-    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
-    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+        # Your code here:
+        # For example, let's assume that if the passenger
+        # is a male, then the passenger survived.
+        #     if passenger['Sex'] == 'male':
+        #         predictions[passenger_id] = 1
+        if passenger['Sex'] == 'female':
+            predictions[passenger_id] = 1
+        else:
+            predictions[passenger_id] = 0
 
-    # YOUR CODE HERE
-    df = DataFrame({'country_name': countries, 'gold': gold, 'silver': silver, 'bronze': bronze})
-    medals = df[['gold', 'silver', 'bronze']]
-    points = np.dot(medals, [4, 2, 1])
-    olympic_points_df = DataFrame({'country_name': countries, 'points': points})
+    return predictions
 
-    return olympic_points_df
-
-print(numpy_dot())
+file_path = 'https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/titanic_data.csv'
+print(simple_heuristic(file_path))

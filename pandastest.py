f4e59484611be33bcb2d09e3c6ddf26ec2dbc871
Liang Peng <penliang@seas.upenn.edu>
2017-01-29 14:58:07 -0500

    cancer lable and sample submission

--- pandastest.py
+++ pandastest.py
@@ -1,20 +1,11 @@
-import json
-import requests
+import pandas as pd
 
-def api_get_request(url):
-    # In this exercise, you want to call the last.fm API to get a list of the
-    # top artists in Spain. The grader will supply the URL as an argument to
-    # the function; you do not need to construct the address or call this
-    # function in your grader submission.
-    #
-    # Once you've done this, return the name of the number 1 top artist in
-    # Spain.
-    data = requests.get(url).text
-    data = json.loads(data)
-    top_artist = data['topartists']['artist'][0]['name']
-    # print(top_artist)
+PATH = r"C:\Users\itoup\Documents\GitHub\tf\lungcancer\stage1_sample_submission.csv\stage1_sample_submission.csv"
+df = pd.read_csv(PATH)
 
-    return top_artist # return the top artist in Spain
+print(df)
 
-url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=spain&api_key=a06291ca756f79ff892f1d17d2f150e5&format=json"
-api_get_request(url)
+PATH = r"C:\Users\itoup\Documents\GitHub\tf\lungcancer\stage1_labels.csv\stage1_labels.csv"
+df = pd.read_csv(PATH)
+
+print(df)

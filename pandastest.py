ea1c1025869c41e3f89b7781440b80a7778790cc
Liang Peng <penliang@seas.upenn.edu>
2017-01-29 16:58:04 -0500

    cancer scan count img

--- pandastest.py
+++ pandastest.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 PATH = "D:/stage1/patients_info.csv"
 df = pd.read_csv(PATH)
 
-plt.hist(df.scans_count, bins = 100)
+plt.hist(df.scans_count, bins = 40)
+plt.title("Frequency of Scan Count")
+plt.xlabel("# of Scans")
+plt.ylabel("# of Patients")
 plt.show()
-
-print(df.describe())

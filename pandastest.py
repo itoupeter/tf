d563624e60814af210fe3e1c29b11c606643c42b
Liang Peng <penliang@seas.upenn.edu>
2017-01-29 16:32:26 -0500

    cancer scan counts

--- pandastest.py
+++ pandastest.py
@@ -4,19 +4,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-INPUT_FOLDER = "D:/stage1/stage1"
-OUTPUT_FOLDER = "D:/stage1"
-patients = os.listdir(INPUT_FOLDER)
-patients.sort()
+PATH = "D:/stage1/patients_info.csv"
+df = pd.read_csv(PATH)
 
-scans_counts = []
+plt.hist(df.scans_count, bins = 100)
+plt.show()
 
-for patient in patients:
-    PATIENT_FOLDER = INPUT_FOLDER + '/' + patient
-    scans = os.listdir(PATIENT_FOLDER)
-    scans_counts.append(len(scans))
-
-df = pd.DataFrame({"id": patients, "scans_count": scans_counts})
-df.to_csv(OUTPUT_FOLDER + "/patients_info.csv")
-
-print(df)
+print(df.describe())

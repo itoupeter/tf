8f20ce827208ba3490c8321a55e4912aa9c57e15
Liang Peng <penliang@seas.upenn.edu>
2017-01-30 17:05:49 -0500

    502 hw1 Q3

--- nptest.py
+++ nptest.py
@@ -1,14 +1,51 @@
 import numpy as np
+import scipy.linalg as linalg
 import matplotlib.pyplot as plt
 
-a = np.random.rand(4, 4)
-b = np.random.rand(4, 1)
+np.set_printoptions(precision = 3, suppress = True)
 
-r1 = np.dot(np.transpose(b), a)
-r1 = np.dot(r1, b)
-print(r1)
+A = np.array([ \
+[8, 11, 3, -2], \
+[3, 2, 2, -1], \
+[-1, 7, -3, 1], \
+[-11, 1, -12, 5]], float)
 
-a = np.transpose(a)
-r1 = np.dot(np.transpose(b), a)
-r1 = np.dot(r1, b)
-print(r1)
+r0 = np.array(A[0,])
+r3 = np.array(A[3,])
+A[3,] = r0
+A[0,] = r3
+
+M1 = np.array([ \
+[1, 0, 0, 0],
+[3./11., 1, 0, 0],
+[-1./11., 0, 1, 0],
+[8./11., 0, 0, 1]], float)
+
+A1 = np.dot(M1, A)
+
+r1 = np.array(A1[1],)
+r3 = np.array(A1[3],)
+A1[3,] = r1
+A1[1,] = r3
+t2 = -A1[2, 1] / A1[1, 1]
+t3 = -A1[3, 1] / A1[1, 1]
+
+M2 = np.array([ \
+[1, 0, 0, 0],
+[0, 1, 0, 0],
+[0, t2, 1, 0],
+[0, t3, 0, 1]], float)
+
+A2 = np.dot(M2, A1)
+
+t3 = -A2[3, 3] / A2[2, 3]
+
+M3 = np.array([ \
+[1, 0, 0, 0],
+[0, 1, 0, 0],
+[0, 0, 1, 0],
+[0, 0, t3, 1]], float)
+
+A3 = np.dot(M3, A2)
+
+M = M3 @ M2 @ M1

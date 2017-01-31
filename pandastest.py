5e07f9ecce93bd1ec1dd77debdd1361717e2b8f7
Liang Peng <penliang@seas.upenn.edu>
2017-01-30 19:51:00 -0500

    ds sql time

--- pandastest.py
+++ pandastest.py
@@ -1,15 +1,16 @@
 import pandas
 import pandasql
 
-
-def max_temp_aggregate_by_fog(filename):
+def avg_weekend_temperature(filename):
     '''
     This function should run a SQL query on a dataframe of
-    weather data.  The SQL query should return two columns and
-    two rows - whether it was foggy or not (0 or 1) and the max
-    maxtempi for that fog value (i.e., the maximum max temperature
-    for both foggy and non-foggy days).  The dataframe will be
-    titled 'weather_data'. You'll need to provide the SQL query.
+    weather data.  The SQL query should return one column and
+    one row - the average meantempi on days that are a Saturday
+    or Sunday (i.e., the the average mean temperature on weekends).
+    The dataframe will be titled 'weather_data' and you can access
+    the date in the dataframe via the 'date' column.
+
+    You'll need to provide  the SQL query.
 
     You might also find that interpreting numbers as integers or floats may not
     work initially.  In order to get around this issue, it may be useful to cast
@@ -18,17 +19,21 @@ def max_temp_aggregate_by_fog(filename):
     write something like where cast(maxtempi as integer) = 76, as opposed to simply
     where maxtempi = 76.
 
+    Also, you can convert dates to days of the week via the 'strftime' keyword in SQL.
+    For example, cast (strftime('%w', date) as integer) will return 0 if the date
+    is a Sunday or 6 if the date is a Saturday.
+
     You can see the weather data that we are passing in below:
     https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/weather_underground.csv
     '''
     weather_data = pandas.read_csv(filename)
 
     q = """
-    SELECT fog, max(maxtempi)
+    SELECT avg(meantempi)
     FROM weather_data
-    GROUP BY fog
+    WHERE cast(strftime('%w', date) as integer) = 0 or cast(strftime('%w', date) as integer) = 6
     """
 
     #Execute your SQL command against the pandas frame
-    foggy_days = pandasql.sqldf(q.lower(), locals())
-    return foggy_days
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends

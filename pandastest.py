f7016b56bb2e9ef54be4e5b2ec37027880ba7de7
Liang Peng <penliang@seas.upenn.edu>
2017-01-30 22:21:25 -0500

    ds merge csv files

--- pandastest.py
+++ pandastest.py
@@ -1,50 +1,36 @@
 import csv
 
-def fix_turnstile_data(filenames):
+def create_master_turnstile_file(filenames, output_file):
     '''
-    Filenames is a list of MTA Subway turnstile text files. A link to an example
-    MTA Subway turnstile text file can be seen at the URL below:
-    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    Write a function that takes the files in the list filenames, which all have the
+    columns 'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn', and consolidates
+    them into one file located at output_file.  There should be ONE row with the column
+    headers, located at the top of the file. The input files do not have column header
+    rows of their own.
 
-    As you can see, there are numerous data points included in each row of the
-    a MTA Subway turnstile text file.
+    For example, if file_1 has:
+    line 1 ...
+    line 2 ...
 
-    You want to write a function that will update each row in the text
-    file so there is only one entry per row. A few examples below:
-    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
-    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
-    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    and another file, file_2 has:
+    line 3 ...
+    line 4 ...
+    line 5 ...
 
-    Write the updates to a different text file in the format of "updated_" + filename.
-    For example:
-        1) if you read in a text file called "turnstile_110521.txt"
-        2) you should write the updated data to "updated_turnstile_110521.txt"
-
-    The order of the fields should be preserved. Remember to read through the
-    Instructor Notes below for more details on the task.
-
-    In addition, here is a CSV reader/writer introductory tutorial:
-    http://goo.gl/HBbvyy
-
-    You can see a sample of the turnstile text file that's passed into this function
-    and the the corresponding updated file by downloading these files from the resources:
-
-    Sample input file: turnstile_110528.txt
-    Sample updated file: solution_turnstile_110528.txt
+    We need to combine file_1 and file_2 into a master_file like below:
+     'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn'
+    line 1 ...
+    line 2 ...
+    line 3 ...
+    line 4 ...
+    line 5 ...
     '''
-    for name in filenames:
-        # your code here
-        output_name = "updated_" + name
-        with open(output_name, 'w') as outfile:
-            writer = csv.writer(outfile, lineterminator = '\n')
-            with open(name, 'r') as infile:
+    with open(output_file, 'w') as master_file:
+        master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
+        writer = csv.writer(master_file, lineterminator = '\n')
+        for filename in filenames:
+            # your code here
+            with open(filename, 'r') as infile:
                 reader = csv.reader(infile)
                 for row in reader:
-                    row_head = row[0:3]
-                    row_len = len(row)
-                    for i in range(3, row_len - 1, 5):
-                        new_row = row[i:i + 5]
-                        writer.writerow(row_head + new_row)
-
-
-fix_turnstile_data(['in.txt'])
+                    writer.writerow(row)

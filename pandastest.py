1d47a8a6f5530fcd86717dbf903b0e41fb528882
Liang Peng <penliang@seas.upenn.edu>
2017-01-30 19:37:20 -0500

    ds sql count cast

--- pandastest.py
+++ pandastest.py
@@ -1,23 +1,37 @@
 import pandas
-import numpy
+import pandasql
 
-def imputation(filename):
-    # Pandas dataframes have a method called 'fillna(value)', such that you can
-    # pass in a single value to replace any NAs in a dataframe or series. You
-    # can call it like this:
-    #     dataframe['column'] = dataframe['column'].fillna(value)
-    #
-    # Using the numpy.mean function, which calculates the mean of a numpy
-    # array, impute any missing values in our Lahman baseball
-    # data sets 'weight' column by setting them equal to the average weight.
-    #
-    # You can access the 'weight' colum in the baseball data frame by
-    # calling baseball['weight']
 
-    baseball = pandas.read_csv(filename)
+def num_rainy_days(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return one column and
+    one row - a count of the number of days in the dataframe where
+    the rain column is equal to 1 (i.e., the number of days it
+    rained).  The dataframe will be titled 'weather_data'. You'll
+    need to provide the SQL query.  You might find SQL's count function
+    useful for this exercise.  You can read more about it here:
 
-    #YOUR CODE GOES HERE
-    mean_weight = numpy.mean(basebal.weight)
-    baseball.weight.fillna(mean_weight, inplace = True)
+    https://dev.mysql.com/doc/refman/5.1/en/counting-rows.html
 
-    return baseball
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be useful to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply
+    where maxtempi = 76.
+
+    You can see the weather data that we are passing in below:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+    SELECT count(*)
+    FROM weather_data
+    WHERE (cast rain as integer) = 1
+    """
+
+    #Execute your SQL command against the pandas frame
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days

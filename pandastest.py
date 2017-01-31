945dbdb6e87d13ceaf27966d862eb8f098296d55
Liang Peng <penliang@seas.upenn.edu>
2017-01-31 12:28:56 -0500

    ds pandas filter rows

--- pandastest.py
+++ pandastest.py
@@ -1,36 +1,25 @@
-import csv
+import pandas
 
-def create_master_turnstile_file(filenames, output_file):
+def filter_by_regular(filename):
     '''
-    Write a function that takes the files in the list filenames, which all have the
-    columns 'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn', and consolidates
-    them into one file located at output_file.  There should be ONE row with the column
-    headers, located at the top of the file. The input files do not have column header
-    rows of their own.
+    This function should read the csv file located at filename into a pandas dataframe,
+    and filter the dataframe to only rows where the 'DESCn' column has the value 'REGULAR'.
 
-    For example, if file_1 has:
-    line 1 ...
-    line 2 ...
+    For example, if the pandas dataframe is as follows:
+    ,C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    1,A002,R051,02-00-00,05-01-11,04:00:00,DOOR,3144335,1088159
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
+    3,A002,R051,02-00-00,05-01-11,12:00:00,DOOR,3144424,1088231
 
-    and another file, file_2 has:
-    line 3 ...
-    line 4 ...
-    line 5 ...
-
-    We need to combine file_1 and file_2 into a master_file like below:
-     'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn'
-    line 1 ...
-    line 2 ...
-    line 3 ...
-    line 4 ...
-    line 5 ...
+    The dataframe will look like below after filtering to only rows where DESCn column
+    has the value 'REGULAR':
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
     '''
-    with open(output_file, 'w') as master_file:
-        master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
-        writer = csv.writer(master_file, lineterminator = '\n')
-        for filename in filenames:
-            # your code here
-            with open(filename, 'r') as infile:
-                reader = csv.reader(infile)
-                for row in reader:
-                    writer.writerow(row)
+
+    turnstile_data = pandas.read_csv(filename)
+    # more of your code here
+    turnstile_data = turnstile_data[turnstile_data.DESCn == 'REGULAR']
+
+    return turnstile_data

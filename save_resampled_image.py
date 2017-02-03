d09a2c4c88dbe9da2ef3d76638298ae9ce20c380
Liang Peng <penliang@seas.upenn.edu>
2017-02-03 14:45:02 -0500

    cancer persist resampled

--- save_resampled_image.py
+++ save_resampled_image.py
@@ -3,6 +3,8 @@ import dicom
 import scipy.ndimage
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas
+import csv
 
 # Load the scans in given folder path
 def load_scan(path):
@@ -56,17 +58,42 @@ def resample(image, scan, new_spacing=[1,1,1]):
 
     return image, new_spacing
 
-DATASET_DIR = "D:/stage1/stage1"
-OUTPUT_DIR = "D:/stage1/mat_resampled/"
+DATASET_DIR = "E:/stage1/stage1"
+OUTPUT_DIR = "D:/stage1/resampled/"
+INFO_FILE = "D:/stage1/patient_info.csv"
 all_patient_ids = os.listdir(DATASET_DIR)
 all_patient_ids.sort()
+df_info = pandas.read_csv(INFO_FILE)
 i = 0
 
 for patient_id in all_patient_ids:
+    # record already resampled
+    if patient_id in df_info.id.values:
+        continue
+
+    # load patient
     DCM_PATH = DATASET_DIR + "/" + patient_id
     OUTPUT_PATH = OUTPUT_DIR + patient_id
     dcm = load_scan(DCM_PATH)
     mat = get_pixels_hu(dcm)
+
+    # resample
     mat_resampled, spacing = resample(mat, dcm)
+
+    # persist resampled matrix
     np.save(OUTPUT_PATH, mat_resampled)
-    print(i, mat.shape, mat_resampled.shape)
+
+    # update record info
+    with open(INFO_FILE, 'a') as file_info:
+        writer = csv.writer(file_info, lineterminator = '\n')
+        depth = mat_resampled.shape[0]
+        width = mat_resampled.shape[1]
+        height = mat_resampled.shape[2]
+        writer.writerow([patient_id, width, height, depth])
+
+    print(patient_id, mat.shape, mat_resampled.shape)
+
+    # loop control
+    i = i + 1
+    if i >= 100:
+        break
